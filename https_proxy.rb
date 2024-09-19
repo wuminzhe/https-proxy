f4f320a26ce8d216c9c49b20c47640d74113d029
Aki Wu <wuminzhe@gmail.com>
2024-09-19 21:09:57 +0800

    update

--- https_proxy.rb
+++ https_proxy.rb
@@ -10,20 +10,50 @@ server = WEBrick::HTTPServer.new(
   SSLVerifyClient: OpenSSL::SSL::VERIFY_NONE,
   SSLCertificate: OpenSSL::X509::Certificate.new(File.read("cert.pem")),
   SSLPrivateKey: OpenSSL::PKey::RSA.new(File.read("key.pem")),
-  SSLCertName: [["CN", WEBrick::Utils.getservername]]
+  SSLCertName: [["AU", WEBrick::Utils.getservername]]
 )
+
+def map_request_class(method)
+  case method
+  when 'GET'
+    Net::HTTP::Get
+  when 'POST'
+    Net::HTTP::Post
+  when 'PUT'
+    Net::HTTP::Put
+  when 'DELETE'
+    Net::HTTP::Delete
+  when 'HEAD'
+    Net::HTTP::Head
+  else
+    raise "Unsupported HTTP method: #{method}"
+  end
+end
 
 # Handle HTTP to HTTP backend
 server.mount_proc '/' do |req, res|
   uri = URI.parse("http://127.0.0.1:3000#{req.path}")
-  proxy_request = Net::HTTP::GenericRequest.new(req.request_method, req.body.nil?, true, uri.request_uri)
+
+# Map request method to appropriate Net::HTTP class
+  request_class = map_request_class(req.request_method)
   
+  # Create a new request instance with the same method as the original request
+  proxy_request = request_class.new(uri)
+  
+  # Pass headers from the original request to the proxy request
+  req.header.each { |key, value| proxy_request[key] = value }
+
+  # If the request has a body (e.g., POST), pass it along as well
+  proxy_request.body = req.body if req.body
+
+  # Send the request to the backend server
   http = Net::HTTP.new(uri.host, uri.port)
   backend_response = http.request(proxy_request)
-  
-  res.body = backend_response.body
+
+  # Set response status, headers, and body based on the backend response
   res.status = backend_response.code.to_i
-  res.content_type = backend_response.content_type
+  res.body = backend_response.body
+  backend_response.each_header { |key, value| res[key] = value }
 end
 
 trap 'INT' do
